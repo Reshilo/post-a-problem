46c42bb46c93a71880bd51fc69885797a2765518
Mladen Plavsic <mladen.plavsic@gmail.com>
2017-09-08 20:42:52 +0200

    Index

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+    <h1>Post-a-Problem</h1>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'index'
 }
 </script>
