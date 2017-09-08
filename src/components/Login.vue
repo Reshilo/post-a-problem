07342d9ce6189f048df308eaf250077246495dc8
Martina Savic <smartinans@gmail.com>
2017-09-08 23:32:52 +0200

    registration page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,14 +1,15 @@
 <template>
   <div>
+   <h1>Prijavi se</h1>
   <div class="form-group">
      <button v-on:click="auth('zendesk')" class="btn btn-primary">Login with ZenDesk</button>
   </div>
 <form>
   <div class="form-group">
-    <input type="text" class="form-control" placeholder="email">
+    <input type="text" class="form-control" id="email-login" name="email" placeholder="email">
   </div>
   <div class="form-group">
-    <input type="text" class="form-control" placeholder="password">
+    <input type="text" class="form-control" id="password-login" name="password" placeholder="lozinka">
   </div>
   <div class="form-group">
     <button type="submit" class="btn btn-primary">uloguj se</button>
