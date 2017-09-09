973496ed6c0b2b3aeac73d3406afc429574d6630
Martina Savic <smartinans@gmail.com>
2017-09-09 11:41:17 +0200

    Fix

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -22,65 +22,41 @@
 </div>
 </template>
 <script>
-export default {
-<<<<<<< HEAD
-data: function () {
-return {
-response: null
-}
-},
-methods: {
-auth: function () {
-if (this.$auth.isAuthenticated()) {
-this.$auth.logout()
-}
-this.response = null
-var this_ = this
-this.$auth.authenticate('zendesk').then(function (authResponse) {
-console.log(authResponse)
-console.log(this_.$auth.isAuthenticated())
-this_.$http.get('https://probprob.zendesk.com/api/v2/users/me.json').then(function (response) {
-this_.response = response
-})
-}).catch(function (err) {
-this_.response = err
-})
-}
-}
-=======
-  data: function () {
-    return {
-      response: null
-    }
-  },
-  created: function () {
-    if (this.$auth.isAuthenticated()) {
-      window.location = '/#/'
-    }
-  },
-  methods: {
-    auth: function () {
-      if (this.$auth.isAuthenticated()) {
-        this.$auth.logout()
+  export default {
+    data: function () {
+      return {
+        response: null
       }
+    },
+    created: function () {
+      if (this.$auth.isAuthenticated()) {
+        window.location = '/#/'
+      }
+    },
+    methods: {
+      auth: function () {
+        if (this.$auth.isAuthenticated()) {
+          this.$auth.logout()
+        }
 
-      this.response = null
+        this.response = null
 
-      var this_ = this
-      this.$auth.authenticate('zendesk').then(function (authResponse) {
-        console.log(authResponse)
-        console.log(this_.$auth.isAuthenticated())
+        var this_ = this
+        this.$auth.authenticate('zendesk').then(function (authResponse) {
+          console.log(authResponse)
+          console.log(this_.$auth.isAuthenticated())
 
-        this_.$http.get('https://probprob.zendesk.com/api/v2/users/me.json', {
-          headers: { Authorization: 'Bearer ' + this_.$auth.getToken() }
-        }).then(function (response) {
-          this_.response = response
+          this_.$http.get('https://probprob.zendesk.com/api/v2/users/me.json', {
+            headers: {
+              Authorization: 'Bearer ' + this_.$auth.getToken()
+            }
+          }).then(function (response) {
+            this_.response = response
+          })
+        }).catch(function (err) {
+          this_.response = err
         })
-      }).catch(function (err) {
-        this_.response = err
-      })
+      }
     }
   }
->>>>>>> b5b4681d0c26f80e400f75a0c68d599fff73822f
-}
 </script>
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,8 +1,12 @@
-<form>
-  <div class="form-group">
-    <input type="checkbox" class="form-control" id="checkbox">
+<template>
+  <div>
+    <form>
+      <div class="form-group">
+        <input type="checkbox" class="form-control" id="checkbox">
+      </div>
+      <div class="form-group">
+        <input type="checkbox" class="form-control" id="checkbox2">
+      </div>
+    </form>
   </div>
-  <div class="form-group">
-    <input type="checkbox" class="form-control" id="checkbox2">
-  </div>
-</form>
+</template>
